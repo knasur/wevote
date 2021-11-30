cc1f7c155ba6bca49f07cc08392e913528cbfe63
knasur <89761454+knasur@users.noreply.github.com>
2021-11-30 18:48:29 +0700

    Add files via upload

---
+++ tempast/script.js
@@ -0,0 +1,105 @@
+function contactsCtrl($scope) {
+
+  $scope.contacts = [
+    {
+      name:"",
+      code:"",
+      year:"",
+      img:"",
+    },{
+      name:"",
+      code:"",
+      year:"",
+      img:"",
+    },{
+      name:"",
+      code:"",
+      year:"",
+      img:"",
+    },{
+      name:"",
+      code:"",
+      year:"",
+      img:"",
+    },
+    
+    
+    
+    {
+      name:"",
+      code:"",
+      year:"",
+      img:"",
+    },{
+      name:"เห็นด้วยกับการฉีดวัคซีนไขว้กันหรือไม่",
+      code:"",
+      year:"",
+      img:"https://images.unsplash.com/photo-1623682687826-fe06bf64e6d8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjF8fHZhY2NpbmV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+    },{
+      name:"เห็นด้วยกับการที่เด็กรุ่นใหม่ย้ายประเทศหรือไม่ ",
+      code:"",
+      year:"",
+      img:"https://images.unsplash.com/photo-1613018274564-bec35a774995?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDV8fGFpcnBvcnR8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+    },
+    {
+    name:"เห็นด้วยกับการทำประกันให้ ไรเดอร์หรือไม่ ",
+    code:"",
+    year:"",
+    img:"https://assets.change.org/photos/4/id/zk/ltIdzKQxndnzzHl-800x450-noPad.jpg?1631284823",
+  },
+  {
+    name:"เห็นด้วยกับการปฏิรูปการศึกษาหรือไม่ ",
+    code:"",
+    year:"",
+    img:"https://www.matichon.co.th/wp-content/uploads/2017/06/%E0%B8%A0%E0%B8%9B-%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B8%A8%E0%B8%B6%E0%B8%81%E0%B8%A9%E0%B8%B2.jpg",
+  },
+  {
+    name:"เห็นด้วยกับการ จัดอุปกรณ์พร้อมอินเตอร์เน็ตคุณภาพดี ",
+    code:"",
+    year:"",
+    img:"https://lh3.googleusercontent.com/proxy/Z2mUdzJrMGV6B20cMr6lgTvsMS4lC1-W-SXjqSUfMrMtiTnHx2qb53R8XWOGa4LOdM8Pb66BHD-3s_4PyTaSUaFcuMsyQiLoJhu4gtB5_geMXt8uguEEDmC4TxI7r0QID1MLo3em6ixe5QzNgpTOxOIaUv71EVx62mMltothp9Pb6ysRHhwcPBl46Zvmjk8uLUyIyOgw7MeVWUOUmeyhhYthg80Muz-YPSvyCys8UwrIc33jPxU5OB2UCF6Q4pGguelOoy-PHMLyz6fGzh9bofHHU20ZCklq-_524qjieWs3nb6qy2ip3eVnlS_D2dBxTDX_jdBB0qwUxm_KDqTG3wcYe-zkbh5J6o-MP0dWyksy9Z3VFXoxgdGqV89BHMrTyw8q2gYSpywrFhkyFv3OZawLpClYhcW1Qbf6KKU7Q6ISVhuFthfCmxGCaZnX1zdBbuPFeIgPOa5m1eARYbiilHajpIDCAdmAV3eKrznzLGezCzAZg6V3nC7xKB7TmkU9XCbpLNjJiBoPqqZrqexwgvY0RCQf83RJYROPNsxZEL8vkzn16AL25WWAmdQkSDbZ6g2fknCBitNV1JTKp1FW7MmZFD154ZBqyjk_xJM5T_Z4SnJ5CniseAHr4tAST_Za_EagvkEl97h5XbPhccyE47moTWFJA8GlGHJceNzLbW-vAvNMI9mk4DyPs7hPS-p0P4BalrzjqwoJUfp0MDZRKqi2TcYq55JA0P_GosjvGikVUkzqX0sAp5mnWWIB4LBjqhQKFNUl3WXtxw-9bBoKThaiL5QYaYyCBWFUuCw6EEJcFmKUBB-x30eTgHd3uPsXOFT7JHAQmUo",
+  },
+  {
+    name:"เห็นด้วยกับการให้คะเเนนนักศึกษา จากยอกไลค์ยอกเเชรหรือไม่ ",
+    code:"",
+    year:"",
+    img:"https://assets.change.org/photos/2/fx/am/DTFXamFSRAsDIeb-800x450-noPad.jpg?1614662346",
+  },
+  {
+    name:"The Amazing Spider Man 3?. ",
+    code:"",
+    year:"",
+    img:"https://www.prachachat.net/wp-content/uploads/2019/08/spider-man-far-from-home.jpg",
+  },
+  {
+    name:"Ralf Rangnick มา Man U!!?? ",
+    code:"",
+    year:"",
+    img:"https://phantom-marca.unidadeditorial.es/66b0cc8983439b3bb05e6ddfe956d3d1/crop/0x26/2048x1176/resize/1320/f/jpg/assets/multimedia/imagenes/2021/11/25/16378644007639.jpg",
+  },
+   {
+    name:"นายก ประยัทธ์ จั้นทรา ควรลาออก!!!",
+    code:"",
+    year:"",
+    img:"https://lh3.googleusercontent.com/proxy/30Jq8TDDGf1NINCI64VNzNhQCSg7ZTDTyubHTRP806azKnjoaghpCvEe8sk2Oo79VcfS693LLtRRGbSNORJbbiGG19tUIMzF2j9kO7L-YCotUGf3KOTzPiZBKTzEvjrx_sUp52eDJ2OniUXpNjqDcLYWKDOgTv26zLagZBKRWs6w",
+  },
+  {
+    name:"ความเห็นต่อค่านิยมการบอกรัก/แสดงความรักในวันวาเลนไทน์",
+    code:"",
+    year:"",
+    img:"https://s.isanook.com/ca/0/rp/r/w850/ya0xa0m1w0/aHR0cHM6Ly9zLmlzYW5vb2suY29tL2NhLzAvdWQvMTg2LzkzMTgyMi92YWxlbnRpbmVzLWRheS5qcGc=.jpg",
+  },{
+    name:"คุณเคยติดโควิดหรือไม่",
+    code:"",
+    year:"",
+    img:"https://images.unsplash.com/photo-1623438787452-56beec0c9d62?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzZ8fGNvdmlkJTIwMTl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+  },{
+    name:"ฉีดวัคซีนแล้วหรือไม่",
+    code:"",
+    year:"",
+    img:"https://media.istockphoto.com/photos/mature-man-taking-a-vaccine-from-his-doctor-picture-id1266494637?b=1&k=20&m=1266494637&s=170667a&w=0&h=EfXVy9i-fKkPBl5vX8D1JNfXXw_EQySze7CMQUqSkik=",
+  },
+    
+  ]
+
+}
---
+++ ต ย พ/เราต้องปฏิรูปการศึกษาที่ล้มเหลวโดยเร็วที่สุด และทั่วโลกกำลังให้ความสำคัญ/script.js
@@ -0,0 +1,39 @@
+var app = angular.module('polApp', []);
+
+app.controller('pollCtrl', function($scope, $http){
+
+//GET VALUES FROM A JSON FILE
+  /*
+  $http.get('poll.json').then(function(response){
+    $scope.test = response.data;
+    $scope.yes = $scope.test[0].yes_votes;
+    $scope.no = $scope.test[0].no_votes;
+    $scope.total = $scope.yes + $scope.no;
+    $scope.yesPer = Math.round(100 *($scope.yes/$scope.total)) ;
+    $scope.noPer = Math.round(100 *($scope.no/$scope.total)) ;
+
+  });
+*/
+  
+//set this values to empty if getting data from A JSON file.
+  $scope.answered = false;
+  $scope.yes = 60;
+  $scope.no = 20;
+  $scope.total = $scope.yes + $scope.no;
+  $scope.yesPer = Math.round(100 *($scope.yes/$scope.total)) ;
+  $scope.noPer = Math.round(100 *($scope.no/$scope.total)) ;
+
+  $scope.addYes = function(){
+    $scope.yes = $scope.yes+1;
+    $scope.total = $scope.yes + $scope.no;
+    $scope.yesPer = Math.round(100 *($scope.yes/$scope.total)) ;
+    $scope.noPer = Math.round(100 *($scope.no/$scope.total)) ;
+  }
+  $scope.addNo = function(){
+    $scope.no = $scope.no+1;
+    $scope.total = $scope.yes + $scope.no;
+    $scope.yesPer = Math.round(100 *($scope.yes/$scope.total)) ;
+    $scope.noPer = Math.round(100 *($scope.no/$scope.total)) ;
+  }
+
+});
